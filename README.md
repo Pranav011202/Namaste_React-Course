6c5cb4b458de3741246ab8c69b6359d5bc581849
Pranav Saluja <pranavsaluja12345@gmail.com>
2024-08-06 20:31:51 +0530

    First commid

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Namaste_React-Course 
+
+
+#parcel 
+ 
+ * dev build 
+ * local server 
+ * HMR - Hote module Replacement 
+ * file watching algorithm written in c++
+ * Caching - Faster builds 
+ * Image Optimization 
+ * Minification 
+ * Bundling 
+ 
+
--- app.js
+++ app.js
@@ -1,14 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const parent = React.createElement("div",
 {id:"parent"},
 [
     React.createElement("div",{id:"child1"},[
-        React.createElement("h1",{},"I'm h1 tag"),
+        React.createElement("h1",{},"This is namaste React "),
         React.createElement("h2",{},"I'm h2 tag"),
     ]),
     React.createElement("div",{id:"child2"},[
         React.createElement("h1",{},"I'm h1 tag"),
         React.createElement("h2",{},"I'm h2 tag"),
-    ])
+    ]),
 ]
 );
 
